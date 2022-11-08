750d2e395fb87275570111ebae91b123b62ed3b7
unknown <megavathsampath@gmail.com>
2022-11-08 17:15:38 +0530

    QuerySelector

--- dom.js
+++ dom.js
@@ -12,11 +12,32 @@
 
 // GetElementByTagName
 
-const li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[2].style.backgroundColor='blue';
+// const li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[2].style.backgroundColor='blue';
 
-for(var i=0 ;i<li.length;i++){
-    li[i].style.fontWeight='bold';
+// for(var i=0 ;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
+
+
+// QuerySelector
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor="green";
+var thirdItem = document.querySelector('.list-group-item: nth-child(3)');
+thirdItem.style.visibility='hidden';
+
+
+// QuerySelectorAll 
+
+var li = document.querySelectorAll('list-group-items');
+li[2].style.color='green';   
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
 }
+
+
+
